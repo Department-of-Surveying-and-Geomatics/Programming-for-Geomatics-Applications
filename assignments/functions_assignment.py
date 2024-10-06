bda1f33aa56a12ac739bf3aa83b874d9a2405158
Surveyor Jr <matingonk@gmail.com>
2024-10-06 16:15:58 +0200

    assignment two starter code

---
+++ assignments/functions_assignment.py
@@ -0,0 +1,18 @@
+def plot_data(my_file):
+    '''
+    This function plots a scatterplot provided with a coordinate text file path 
+
+    Parameters
+    ----------
+    my_file : str (REQUIRED)
+        The path to the text file containing the coordinates
+
+    Returns
+    -------
+    Scatterplot
+    '''
+    scatter_plot = None 
+    """
+    ==> Write your code here <==
+    """
+    return scatter_plot
